8960bec81640f61f6a6081924681a425a22989e5
olvera93 <olvera9302@outlook.com>
2024-11-25 20:47:49 -0600

    [ADD] Unit tests for client sessions methods for the success cases

--- src/test/kotlin/com/olvera/groceries/service/ClientSessionServiceImplTest.kt
+++ src/test/kotlin/com/olvera/groceries/service/ClientSessionServiceImplTest.kt
@@ -7,7 +7,9 @@ import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContext
@@ -42,6 +44,29 @@ class ClientSessionServiceImplTest {
     }
 
 
+    @Test
+    fun `when retrieve authentication is called then expect matching objects`() {
+        val actualResult: Authentication? = objectUnderTest.retrieveAuthentication()
 
+        assertEquals(mockAuthentication, actualResult)
+    }
 
+    @Test
+    fun `when get authenticated user is called then expect the response to match global user field`(){
+
+        every { mockAuthentication.principal } returns user
+
+        val actualResult: AppUser = objectUnderTest.getAuthenticatedUser()
+
+        assertEquals(user, actualResult)
+    }
+
+    @Test
+    fun `when find current session user is called then expect the response to match global user field`() {
+        every { mockUserRepository.findByAppUsername(any()) } returns user
+
+        val actualResult: AppUser = objectUnderTest.findCurrentSessionUser()
+
+        assertEquals(user, actualResult)
+    }
 }
